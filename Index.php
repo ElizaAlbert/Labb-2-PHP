479a3101c2c2336f673875a1d6d01de5fa23dccb
Emil Backlund <emilbacklund@outlook.com>
2020-05-26 15:03:53 +0200

    Basic table for all members and added buttons

--- Index.php
+++ Index.php
@@ -4,17 +4,21 @@ $servername = "localhost:8889";
 $username = "root";
 $password = "root";
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=labb2-php", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch (PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+
+$conn = new PDO("mysql:host=$servername;dbname=labb2-php", $username, $password);
+
+
+$members = [];
+foreach ($conn->query("SELECT * FROM members") as $row) {
+  $members[] = $row;
 }
 
 
-$test = ''
+
+// $sth = $conn->prepare($members);
+// $sth->execute()
+
+
 
 ?>
 
@@ -25,10 +29,40 @@ $test = ''
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>IK Svalan</title>
+  <link rel="stylesheet" href="./assets/style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 
 <body>
-  //
+  <table>
+    <tr>
+      <th>Nr</th>
+      <th>Förnamn</th>
+      <th>Efternamn</th>
+      <th>Betald</th>
+      <th>Aktivitet</th>
+      <th>Lag</th>
+      <th>Ta bort</th>
+      <th>Redigera</th>
+    </tr>
+    <?php
+    foreach ($members as $member) {
+      echo "<tr>";
+      echo "<td>" . $member[0] . "</td>";
+      echo "<td>" . $member[1] . "</td>";
+      echo "<td>" . $member[2] . "</td>";
+      echo "<td>" . $member[3] . "</td>";
+      echo "<td>" . $member[4] . "</td>";
+      echo "<td>" . $member[5] . "</td>";
+      echo "<td><button type='button'><i class='material-icons'>clear</i></button></td>";
+      echo "<td><button type='button'><i class='material-icons'>add_circle_outline</i></button></td>";
+      echo "</tr>";
+    }
+
+
+    ?>
+  </table>
+
 </body>
 
 </html>
