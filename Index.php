b030909c20ee38235ab31b5ca24d876f1df6bfce
Emil Backlund <emilbacklund@outlook.com>
2020-05-26 13:21:15 +0200

    DB connection done

--- Index.php
+++ Index.php
@@ -1,5 +1,19 @@
 <?php
-//
+
+$servername = "localhost:8889";
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
 ?>
 
 <!DOCTYPE html>
